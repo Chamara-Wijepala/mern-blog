3fdafa7fee202c3dba694827a47139fe93994d02
Chamara-Wijepala <mr.worf777@gmail.com>
2023-11-15 22:46:58 +0530

    Add blog page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Blog from './pages/Blog';
 
 export default function App() {
 	return (
 		<div className="container mx-auto p-4">
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/blogs/:id" element={<Blog />} />
 			</Routes>
 		</div>
 	);
---
+++ client/src/pages/Blog.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+
+export default function Blog() {
+	const [blog, setBlog] = useState({});
+	const { id } = useParams();
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/blogs/${id}`)
+			.then((res) => {
+				setBlog(res.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
+	return (
+		<>
+			<div className="flex gap-4 mb-8">
+				<Link to="/">
+					<button className="py-1 px-4 rounded-md border-gray-400 border">
+						Back
+					</button>
+				</Link>
+
+				<button className="py-1 px-4 ml-auto rounded-md border-gray-400 border">
+					Edit
+				</button>
+				<button className="py-1 px-4 rounded-md bg-red-400 border-gray-400 border">
+					Delete
+				</button>
+			</div>
+
+			<div>
+				<h2 className="text-3xl mb-4">{blog.title}</h2>
+				<p className="text-lg">{blog.description}</p>
+			</div>
+		</>
+	);
+}
